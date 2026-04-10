1e50b17f3a04a3d06980e264ba4441d273320b3d
storm-break-R <chauhanpiyush34@gmail.com>
2026-04-10 14:15:08 +0530

    added route for 404 page not found

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import SearchPage from "./components/SearchPage";
 import React from "react";
+import PageNotFound from "./components/PageNotFound";
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/search" element={<SearchPage />} />
+            <Route path="*" element={<PageNotFound />} />
         </Routes>
     );
 };
